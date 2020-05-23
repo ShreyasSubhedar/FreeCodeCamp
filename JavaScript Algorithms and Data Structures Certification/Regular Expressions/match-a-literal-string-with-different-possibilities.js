26c13955f97aee739185128a5f9cc97cd7b9cc6d
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-05-23 12:40:07 +0530

    REGEX_

---
+++ JavaScript Algorithms and Data Structures Certification/Regular Expressions/match-a-literal-string-with-different-possibilities.js
@@ -0,0 +1,3 @@
+let petString = "James has a pet cat.";
+let petRegex = /John has a pet dog.|Liz has a pet cat.|Alice has a pet fish.|Emma has a pet bird./; // Change this line
+let result = petRegex.test(petString);
