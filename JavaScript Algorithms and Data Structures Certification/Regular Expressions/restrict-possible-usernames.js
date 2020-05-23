c243aa010430df56c35efcaaa63120c1675259b3
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-05-23 20:15:13 +0530

    REGEX_CONTINUE

---
+++ JavaScript Algorithms and Data Structures Certification/Regular Expressions/restrict-possible-usernames.js
@@ -0,0 +1,12 @@
+// You need to check all the usernames in a database. Here are some simple rules that users have to follow when creating their username.
+
+// 1) Usernames can only use alpha-numeric characters.
+
+// 2) The only numbers in the username have to be at the end. There can be zero or more of them at the end. Username cannot start with the number.
+
+// 3) Username letters can be lowercase and uppercase.
+
+// 4) Usernames have to be at least two characters long. A two-character username can only use alphabet letters as characters.
+let username = "JackOfAllTrades";
+let userCheck =  /^[a-z]([0-9][0-9]+|[a-z]+\d*)$/i; // Change this line
+let result = userCheck.test(username);
---
+++ JavaScript Algorithms and Data Structures Certification/Regular Expressions/specify-exact-number-of-matches.js
@@ -0,0 +1,3 @@
+let timStr = "Timmmmber";
+let timRegex = /^Ti[m]{4}ber/; // Change this line
+let result = timRegex.test(timStr);
