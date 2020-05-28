79fabe2ddf6cd3cbe3385b64ae50f515b0f159d3
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-05-28 16:34:10 +0530

    FCC_CONT

---
+++ JavaScript Algorithms and Data Structures Certification/Functional Programming/split-a-string-into-an-array-using-the-split-method.js
@@ -0,0 +1,8 @@
+function splitify(str) {
+  // Only change code below this line
+  var re =/\s|-|,|\./;
+    return str.split(re)
+
+  // Only change code above this line
+}
+console.log(splitify("Hello World,I-am code"));
