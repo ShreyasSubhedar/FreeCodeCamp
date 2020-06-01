592cec2aeac0355dc1ed11990a021c9233ee19ed
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-01 23:33:19 +0530

    REACT_CONTINUE

---
+++ Frontend Libraries/React/add-comments-in-jsx.jsx
@@ -0,0 +1,8 @@
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+        {/* LOlz you are asking me to type a comment*/}
+
+  </div>
+);
---
+++ Frontend Libraries/React/learn-about-self-closing-jsx-tags.jsx
@@ -0,0 +1,10 @@
+const JSX = (
+  <div>
+    
+
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+
+  </div>
+);
