22e54c900cf5a619e0ddc15cf157f3d747bc9d7f
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-08 17:22:16 +0530

    QUOTE_MACHINE

--- Frontend Libraries/Projects/hello-world/src/App.js
+++ Frontend Libraries/Projects/hello-world/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="row">
+        <div className="col-2">
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="col-8 text-center mt-5 rounded border">
+        <blockquote>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+</blockquote>
+        </div>
+        <div className="col-2">
+        </div>
       </div>
     );
   }
