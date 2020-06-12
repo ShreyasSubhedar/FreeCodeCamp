d44f3eb0a0e121f7c2d9aa75752957290da6297f
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-12 23:12:09 +0530

    CAUCULATOR_11/16

--- Frontend Libraries/Projects/calculator/src/index.js
+++ Frontend Libraries/Projects/calculator/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import 'bootstap/dist/css/bootstrap.min.css' 
+import 'bootstrap/dist/css/bootstrap.min.css' 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
