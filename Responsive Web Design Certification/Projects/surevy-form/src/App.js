a1b1b686ab45963d05a03a12fea0e937c11cee62
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-14 22:26:23 +0530

    SURVEY_FORM

--- Responsive Web Design Certification/Projects/surevy-form/src/App.js
+++ Responsive Web Design Certification/Projects/surevy-form/src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="container">
+  <div className="row">
+    <div className="col-sm">
+    </div>
+    <div className="col-sm mt-5 border rounded">
+      <form action="" method="POST" className="form-control">
+        <input type="text" className="form"
+        </form> 
+    </div>
+    <div className="col-sm">
+      
+    </div>
+  </div>
+</div>
     );
   }
 }
