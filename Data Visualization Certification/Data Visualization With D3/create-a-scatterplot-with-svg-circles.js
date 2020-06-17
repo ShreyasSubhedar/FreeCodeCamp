878c1793c6e5799a407f34333490b57802688a4e
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-17 19:26:58 +0530

    D3_60%

---
+++ Data Visualization Certification/Data Visualization With D3/create-a-scatterplot-with-svg-circles.js
@@ -0,0 +1,38 @@
+<body>
+  <script>
+    const dataset = [
+                  [ 34,    78 ],
+                  [ 109,   280 ],
+                  [ 310,   120 ],
+                  [ 79,    411 ],
+                  [ 420,   220 ],
+                  [ 233,   145 ],
+                  [ 333,   96 ],
+                  [ 222,   333 ],
+                  [ 78,    320 ],
+                  [ 21,    123 ]
+                ];
+
+
+    const w = 500;
+    const h = 500;
+
+    const svg = d3.select("body")
+                  .append("svg")
+                  .attr("width", w)
+                  .attr("height", h);
+
+    svg.selectAll("circle")
+       // Add your code below this line
+       .data(dataset)
+       .enter()
+        .append("circle")
+        .attr("x",(i,j)=>{i*10})
+        .attr("y",(i,j)=>{j*10})
+        .attr("r",10)
+
+
+       // Add your code above this line
+
+  </script>
+</body>
