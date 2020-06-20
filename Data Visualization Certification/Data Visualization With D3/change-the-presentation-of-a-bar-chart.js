c600d2e3ed1f29bddc699d89f6230c89d5072cbd
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-06-20 12:24:44 +0530

    JSON_API_&_AJAX_COMPLETE

--- Data Visualization Certification/Data Visualization With D3/change-the-presentation-of-a-bar-chart.js
+++ Data Visualization Certification/Data Visualization With D3/change-the-presentation-of-a-bar-chart.js
@@ -1,3 +1,5 @@
+    <script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+
 <style>
   .bar {
     width: 25px;
@@ -12,14 +14,22 @@
 </style>
 <body>
   <script>
-    const dataset = [12, 31, 22, 17, 25, 18, 29, 14, 9];
+  var d =[]
+fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
+        .then(response=>response.json())
+        .then(data=>{
+        for(var i in data.data){
+          d.push(data.data[i][1])
+        }
 
-    d3.select("body").selectAll("div")
-      .data(dataset)
-      .enter()
-      .append("div")
-      .attr("class", "bar")
-      .style("margin","2px")
-      .style("height", (d) => (d*10 + "px"))
+        });
+
+        d3.select("body").selectAll("div")
+        .data(d)
+        .enter()
+        .append("div")
+        .attr("class", "bar")
+        .style("margin","2px")
+        .style("height", (d) => (d*10 + "px"))
   </script>
 </body>
